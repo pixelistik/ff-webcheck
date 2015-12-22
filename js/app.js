d8597e9c9977084c8a410bb9e37c06b2642c5980
Pixelistik <code@pixelistik.de>
2015-12-22 16:43:43 +0100

    Main model should be a separate module

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 "use strict";
-var FfWebcheck = require("./models.js");
+var FfWebcheck = require("./ffwebcheck.js");
 var ko = require("knockout");
 var ffWebcheck = new FfWebcheck();
 ko.applyBindings(ffWebcheck);
